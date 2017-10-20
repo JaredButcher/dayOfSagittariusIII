4de07b249ace6f199a1a4135687b9ab36197cdf6
Jared Butcher <jedimaster1998@hotmail.com>
2017-10-19 21:33:36 -0400

    async io stuff

--- Server/sockServer.py
+++ Server/sockServer.py
@@ -1,18 +1,50 @@
 import socket
+import asyncio
 import threading
-
-def accept(server):
-    while True:
-        conn, address = server.accept()
-        print("Connection established")
         
+clients = []
 
 def start(port, data):
-    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serv.setblocking(1)
-    serv.bind(('',port))
-    serv.listen(1)
-    accept(serv)
+    loop = asyncio.get_event_loop()
+    serv = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
+    server = loop.run_until_complete(serv)
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+async def connectLoop(port):
+    serv = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
+    serv.bind(('', port))
+    serv.listen(4)
+    while True:
+        conn, address = await serv.accept()
+        clients.append(client(conn))
+
+
+class client:
+    def __init__(self, conn):
+        self.conn = conn
+    
+    async def beginReceiveLoop():
+        data = await comm.recv(1024)
+        
+
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print("Received %r from %r" % (message, addr))
+
+    print("Send: %r" % message)
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the client socket")
+    writer.close()
     
 '''
 class ComControl:
